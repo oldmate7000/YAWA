ae33f7fa3a7c4b4e5ec94feff30b68347ccadad9
Bryce Leath <bryce.leath@gmail.com>
2020-03-24 12:45:43 +1100

    updated auth

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ const ObjectID      = require('mongodb').ObjectID;
 
 module.exports = function (app, db) {
   passport.serializeUser((user, done) => {
-      console.log(user)
+      // console.log(user)
       return done(null, user._id);
     });
   
@@ -18,7 +18,7 @@ module.exports = function (app, db) {
   
   passport.use(new LocalStrategy(function(username, password, done) {
     db.collection('users').findOne({ username: username }, function (err, user) {
-      console.log('User '+ username +' attempted to log in.');
+      // console.log('User '+ username +' attempted to log in.');
       if (err) { return done(err); }
       if (!user) { return done(null, false); }
       if (!bcrypt.compareSync(password, user.password)) { return done(null, false); }
